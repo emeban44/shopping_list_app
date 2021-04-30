7aa408f46ee35aaf558eeeb9fbc383d8659dfdff
emeban44 <emi.cazorla@hotmail.com>
2021-04-30 16:58:01 +0200

    home page bottom bar design + fonts

---
+++ lib/screens/add_item_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AddItemScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        child: Text('Press'),
+        onPressed: () {},
+      ),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list_app/screens/add_item_screen.dart';
+import 'package:shopping_list_app/screens/collected_items_screen.dart';
+import 'package:shopping_list_app/screens/list_items_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Widget> _pages = [
+    ListScreen(),
+    AddItemScreen(),
+    CollectedItemsScreen(),
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'MyShopping List',
+          style: TextStyle(
+            fontFamily: 'NewTegomin',
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: _pages[_selectedPageIndex],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(
+              color: Theme.of(context).primaryColor,
+              width: 0.75,
+            ),
+          ),
+        ),
+        child: BottomNavigationBar(
+          //  backgroundColor: Colors.blue.shade100,
+          currentIndex: _selectedPageIndex,
+          onTap: _selectPage,
+          backgroundColor: Colors.green.shade50,
+          unselectedLabelStyle: TextStyle(fontSize: 15),
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.list_alt_rounded,
+                size: 36,
+              ),
+              label: 'Shopping List',
+            ),
+            BottomNavigationBarItem(
+              icon: Container(
+                width: 100,
+                padding: const EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.blue.shade200,
+                      Colors.green.shade200,
+                    ],
+                  ),
+                  borderRadius: BorderRadius.circular(50),
+                  border: Border.all(
+                    color: Theme.of(context).primaryColor,
+                    width: 0.5,
+                  ),
+                ),
+                child: Icon(
+                  Icons.add_shopping_cart_rounded,
+                  size: 40,
+                ),
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              activeIcon: Icon(
+                Icons.check_box_rounded,
+                size: 36,
+              ),
+              icon: Icon(
+                Icons.check_box_outlined,
+                size: 36,
+              ),
+              label: 'Collected',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
